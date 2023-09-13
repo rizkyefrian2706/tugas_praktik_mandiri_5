048b32831daecb9d1add44807e27859ffbb2d6e0
ATS.Rizky Efrian <ats.rizkye@xl.co.id>
2023-09-13 15:03:07 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Halo Altera')
+        ), 
+        body: const Center( 
+        child: Column( 
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Nama saya adalah Rizky Efrian',
+            ), 
+          ],
+        ),
+      ),
+      )
+    );
+  }
+} 
